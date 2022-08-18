8ea642eac616706aef8c0d2e316fb812c11fa8c9
wl <wl@xydata.cn>
2022-08-18 09:58:53 +0800

    fix: first

---
+++ leetcode/src/com/yuan/onlineprograming/LC001.java
@@ -0,0 +1,6 @@
+package com.yuan.onlineprograming;/**
+ * @author wl
+ * @create 2022/08/16
+ */
+public class LC001 {
+}
---
+++ leetcode/src/com/yuan/onlineprograming/LC002.java
@@ -0,0 +1,6 @@
+package com.yuan.onlineprograming;/**
+ * @author wl
+ * @create 2022/08/17
+ */
+public class LC002 {
+}
